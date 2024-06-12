f39f336143eea9201f63cf6fe232ea67d4836ffc
chicken-1 <nguyenthanhan3703@gmai.com>
2024-06-12 14:31:19 +0700

    complete I2C, led, sw

--- Fall detection/I2C.h
+++ Fall detection/I2C.h
@@ -1,7 +1,8 @@
 #include "MKL46Z4.h"
 
-void InitI2C(void);
+void initI2C(void);
+void initINT2(void);
 void writeI2C(uint8_t data);
 void configMMA84512Q(void);
 void transmit(uint8_t reg_add, uint8_t data);
-uint8_t recieve(uint8_t reg_add);
+uint8_t receive(uint8_t reg_add);
